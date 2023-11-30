9a7ccdbbd36ae1a677d4146b665735721039e556
admin <jshosseini643@gmail.com>
2023-11-30 12:21:26 -0800

    add OnBordingScreen

--- app/src/main/java/com/example/newsapplication/MainActivity.kt
+++ app/src/main/java/com/example/newsapplication/MainActivity.kt
@@ -3,14 +3,18 @@ package com.example.newsapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.newsapplication.presentation.onbording.OnBordingScreen
 import com.example.newsapplication.ui.theme.NewsApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,13 +23,11 @@ class MainActivity : ComponentActivity() {
         installSplashScreen()
         setContent {
             NewsApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+
+                Box(modifier=Modifier.background(color = MaterialTheme.colorScheme.background)){
+                    OnBordingScreen()
+            }
+
             }
         }
     }
@@ -43,6 +45,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     NewsApplicationTheme {
-        Greeting("Android")
-    }
+        Box(modifier=Modifier.background(color = MaterialTheme.colorScheme.background)){
+            OnBordingScreen()
+        }    }
 }
